fe5f5339330ac4e86018e150cea7bd656e74562b
Temich93s <nil>
2021-09-30 09:06:13 +1000

    Пройдена тема: Синтаксис замыкающего выражения

--- Closures/main.swift
+++ Closures/main.swift
@@ -15,8 +15,14 @@ print("\n//Метод sorted")
 
 let someArray1 = ["q", "w", "e", "r", "t", "y", "u", "i"]
 
-func someFunc1(_ a: String, _ b: String) -> (Bool) {
+func someFunc1(_ a: String, _ b: String) -> Bool {
     return a > b
 }
 
 print(someArray1.sorted(by: someFunc1))
+
+
+//MARK: Синтаксис замыкающего выражения
+print("\n//Синтаксис замыкающего выражения")
+
+print(someArray1.sorted(by: {(a: String, b: String) -> Bool in return a > b }))
